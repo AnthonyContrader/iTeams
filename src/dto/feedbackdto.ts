571c8fb35e4fbf867baffe1d4738b98eaa96c111
emanuelferrucci <emanuel.ferrucci@gmail.com>
2019-08-20 17:00:38 +0200

    feedback e team

---
+++ src/dto/feedbackdto.ts
@@ -0,0 +1,14 @@
+import { UserDTO } from './userdto';
+
+export class FeedbackDTO {
+    id: number;
+    sportDTO: ;
+    userDTO: UserDTO;
+    creatorDTO: UserDTO;
+    rate: number;
+
+
+
+
+
+}
---
+++ src/dto/teamdto.ts
@@ -0,0 +1,11 @@
+import { HasEventTargetAddRemove } from 'rxjs/internal/observable/fromEvent';
+import { UserDTO } from './userdto';
+
+export class TeamDTO {
+
+    id: number;
+    name: string;
+    eventDTO: ;
+    componentsDTO: UserDTO;
+    
+}
