ad345fe181b9cb0abebc855acc767c13c512dff8
scastaldo03 <s.castaldo03@gmail.com>
2019-08-21 11:10:43 +0200

    sport e event service up

--- src/service/event.service.ts
+++ src/service/event.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { EventDTO } from 'src/dto/eventdto';
+import { AbstractService } from './abstractservice';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class EventService extends AbstractService<EventDTO> {
 
-  constructor() { }
-}
+    constructor(http: HttpClient) {
+      super(http);
+      this.type = 'event';
+    }
+  
+    
+  }
--- src/service/sport.service.ts
+++ src/service/sport.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AbstractService } from './abstractservice';
+import { SportDTO } from 'src/dto/sportdto';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class SportService {
+export class SportService extends AbstractService<SportDTO>{
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http);
+    this.type = 'sport';
+  }
 }
