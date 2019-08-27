7e7911c906261e8caf70c7de8be159db7d395c83
emanuelferrucci <emanuel.ferrucci@gmail.com>
2019-08-27 17:52:59 +0200

    created events

--- src/app/user/sports/sports.component.ts
+++ src/app/user/sports/sports.component.ts
@@ -3,6 +3,7 @@ import { SportService } from 'src/service/sport.service';
 import { SportDTO } from 'src/dto/sportdto';
 import { UserDTO } from 'src/dto/userdto';
 import { UserService } from 'src/service/user.service';
+import { BOOL_TYPE } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-sports',
@@ -10,12 +11,14 @@ import { UserService } from 'src/service/user.service';
   styleUrls: ['./sports.component.css']
 })
 export class SportsComponent implements OnInit {
+  indice: number;
   users : UserDTO[];
   user: UserDTO;
   sporttoupdate: SportDTO;
   sports: SportDTO[];
   sporttoinsert: SportDTO = new SportDTO();
-  sportL: SportDTO =new SportDTO();
+  sportL: SportDTO[];
+  bb : Boolean=true;
   constructor(private service: SportService, private uService: UserService) { }
 
   ngOnInit() {
@@ -28,14 +31,32 @@ export class SportsComponent implements OnInit {
   }
 
   liked(sport: SportDTO){
-    console.log("indice:"+this.user.like.indexOf(sport));
-    this.user.like.splice(this.user.like.indexOf(sport), 1);
-    console.log("in ELSE");
-    this.user.like.push(sport);
-    this.uService.update(this.user).subscribe(()=>this.user);
-    localStorage.setItem('currentUser', JSON.stringify(this.user));
-    console.log("utente corrente: "+localStorage.getItem('currentUser'));
-    this.service.update(sport).subscribe(() => this.getSports());
+    console.log("nome sport: "+sport.name);
+    console.log(this.user.like.indexOf(sport));
+    console.log("lunghezza array"+this.user.like.length);
+    this.sportL=this.user.like;
+    console.log("toStrong: "+this.user.like.toString);
+    sp : SportDTO;
+    
+    for(let sp of this.sportL){
+      console.log("sportName:"+sport.name);
+      console.log("sportName:"+sp.name);
+      if(sp.name==sport.name){
+        this.bb=false;
+        console.log("nell'if del for");
+      }
+    }
+    if(!this.bb){
+      console.log("in IF");
+    }
+    else{
+      console.log("in ELSE");
+      this.user.like.push(sport);
+      this.uService.update(this.user).subscribe(()=>this.user);
+      localStorage.setItem('currentUser', JSON.stringify(this.user));
+      console.log("utente corrente: "+localStorage.getItem('currentUser'));
+      //this.service.update(sport).subscribe(() => this.getSports());
+    }
   }
 
   delete(sport: SportDTO) {
@@ -50,6 +71,16 @@ export class SportsComponent implements OnInit {
     this.service.insert(sport).subscribe(() => this.getSports());
   }
 
+  /*
+  joined(sport: SportDTO) : boolean{
+    this.uSports=this.user.like;
+
+    if(this.uSports.includes(sport))
+      return false;
+    else
+      return true;
+  }
+  */
   clear(){
     this.sporttoinsert = new SportDTO();
   }
